3ae41917abc187e391a3bd6e65c9a2db3af5fea6
serhiykobyakov <photokobyakov@gmail.com>
2022-07-12 15:24:51 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# CDROM_actuator_automatization
+## Automation of a CDROM linear actuator using Arduino
+
+### What it does
+
+The software has been tested for errors, stability and speed (bugs free is not guaranteed, see the licence).
+
+### Install
+
+1. Make directory "ShutterCD" in the sketchbook directory on your PC (it is "Arduino" by default, check the preferences in Arduino IDE).
+2. Put the ShutterCD.ino into "ShutterCD" directory.
+3. Open Arduino IDE and set your Arduino board
+4. Check the sketch for errors and upload the sketch to the board.
+
+### How It Works
+The stepper motor in CDROM actuator can be driven by some low power driver, for example [this one](https://learn.sparkfun.com/tutorials/easy-driver-hook-up-guide/all).
+
+Arduino board can drive the stepper driver.
+
+In this particular case I needed just two extreme positions of the linear actuator corresponding to the stated of the optical shutter: "open" and "close".
+
+Here I have only the Arduino sketch without all the other stuff - the circuitry and the case.
+
+The device starts from finding the initial position using endstop.
+
+### How To Setup The Device
+When you have the device assembled - you need to check for two things:
+1. What is the positive direction of motion - it must be the one when the carriage moves away from the endstop.
+2. How many steps is the travel range of your linear actuator - at the maximum it must stop at the end of the range in order to not hit the wall (maybe the good idea is to put another endstop there). Check for the number and then correct the sketch accordingly.
+
+
+### How To Use
+To open the optical shutter one have to send "o" to serial port, to close it - send "c".
+Arduino sends back zero when the process is finished.
+
+
+### Contact
+For reporting [bugs, suggestions, patches](https://github.com/serhiykobyakov/CDROM_optical_shutter/issues)
+
+
+### License
+The project is licensed under the [MIT license](https://github.com/serhiykobyakov/CDROM_optical_shutter/blob/main/LICENSE)
---
+++ ShutterCD.ino
@@ -0,0 +1,123 @@
+// ShutterCD
+//
+// Copyright (C) 2022 Serhiy Kobyakov
+//
+
+
+#include <AccelStepper.h>
+
+// Dynamics of the stepper motor
+#define TheSpeed 30000.0
+#define TheAcceleration 19000.0
+
+// The stepper driver pins
+#define STEPPER1_DIR_PIN 3
+#define STEPPER1_STEP_PIN 2
+#define MotorDisable 4
+
+// Endstop pin
+#define theEndStop A0
+
+long long int steps; // motor step position
+int count;           // just counter variable
+
+int theMax=1950;     // maximum number of steps 
+                     // for this particular linear actuator
+                     // need to be checked for each new device!
+
+// Define some steppers and the pins the will use
+AccelStepper stepper1(AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
+
+
+void setup()
+{
+  pinMode(MotorDisable, OUTPUT);
+  digitalWrite(MotorDisable, HIGH);
+  stepper1.setMaxSpeed(TheSpeed);
+  stepper1.setAcceleration(TheAcceleration);
+  Serial.begin(115200);
+  while (!Serial);
+  shutterInit();
+}
+
+
+void closeShutter()
+{
+  digitalWrite(MotorDisable, LOW);
+  delay(20);
+  stepper1.moveTo(theMax);
+  while (stepper1.distanceToGo() != 0) stepper1.run();
+  digitalWrite(MotorDisable, HIGH);
+}
+
+
+void openShutter()
+{
+  digitalWrite(MotorDisable, LOW);
+  delay(20);
+  stepper1.moveTo(0);
+  while (stepper1.distanceToGo() != 0) stepper1.run();
+  digitalWrite(MotorDisable, HIGH);
+}
+
+
+void shutterInit()
+{
+  digitalWrite(MotorDisable, LOW);
+  
+  if (hitEndStop()) {  
+    stepper1.moveTo(stepper1.currentPosition() + 130);
+    while (stepper1.distanceToGo() != 0) stepper1.run();  
+  }
+
+  count=0;
+  do {
+    stepper1.moveTo(stepper1.currentPosition() - 1);
+    while (stepper1.distanceToGo() != 0) stepper1.run();
+    count++;
+  } while ( not hitEndStop());
+  
+  delay(100);
+  stepper1.moveTo(stepper1.currentPosition() + 60);
+  while (stepper1.distanceToGo() != 0) stepper1.run();
+  stepper1.setCurrentPosition(0);
+  digitalWrite(MotorDisable, HIGH);
+}
+
+
+boolean hitEndStop()
+// check if the carriage hit the endstop
+{
+  delay(8);
+  if (analogRead(theEndStop) < 100) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+void loop()
+{
+  if (Serial.available() > 0 ) {
+    char data_in = (char) Serial.read();
+
+    switch (data_in) {
+    case '?':  // print identification string
+      delay(4);
+      Serial.println("Shutter CD");
+      break;
+    case 'o':  // open shutter
+      openShutter();
+      Serial.println("0");
+      break;
+    case 'c':  // close shutter
+      closeShutter();
+      Serial.println("0");
+      break;
+    default:  // do nothing if other input happens
+        break;
+    }// end of switch    
+
+  }// end of Serial.available
+}
